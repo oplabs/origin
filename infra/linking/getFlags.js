296643ce267522f2c0c63e907b5403f537d1b2f5
Yu Pan <yupan@originprotocol.com>
2019-08-16 13:56:47 -0700

    fix get flags

--- infra/linking/getFlags.js
+++ infra/linking/getFlags.js
@@ -2,10 +2,16 @@ import Webrtc from './src/logic/webrtc'
 
 const webrtc = new Webrtc()
 async function getFlags(ethAddress) {
-  return await webrtc.getRedis(`flag.${ethAddress}`)
+  return new Promise((resolve, reject) => webrtc.redis.lrange(`flag.${ethAddress}`, 0, -1, (err, items) => {
+    if (err) {
+      reject('empty list')
+    } else {
+      resolve(items)
+    }
+  }))
 }
 
-getFlags(process.argv[1]).then(result => {
-  console.log(`flags ${result}`)
+getFlags(process.argv[2]).then(result => {
+  console.log(`flags[${process.argv[2]}]:`, result)
   process.exit(1)
 })
